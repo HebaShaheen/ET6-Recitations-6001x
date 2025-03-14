93616bf483eacd495207788f610a5611967f4dc6
Heba Shaheen <shaheenheba2000@gmail.com>
2025-03-14 04:07:05 +0300

    _

--- session_01/problem_1.py
+++ session_01/problem_1.py
@@ -5,11 +5,13 @@ Created on Fri Jan 24 11:39:16 2025
 
 --- 1: Working with Variables in Python ---
 
-Variables in Python serve as a way to label and store values for later use. For example, to define a variable named x and assign it the value 10, you can use the = symbol like this:
+Variables in Python serve as a way to label and store values for later use.
+For example, to define a variable named x and assign it the value 10,
+you can use the = symbol like this:
 
 x = 10 
 
-Once assigned, you can reference x instead of repeatedly typing the value 5 in your code.
+Once assigned, you can reference x instead of repeatedly typing the value 10 in your code.
 Important Note: In Python, the = symbol is used for assignment, not for checking equality. To test this out, follow the instructions below by replacing the placeholders with your own code.
 
 Instructions
@@ -17,4 +19,4 @@ Instructions
 2. Verify your variable by adding the line print(savings) to the script and running it.
 """
 savings = 100
-print(savings) 
+print(savings)
